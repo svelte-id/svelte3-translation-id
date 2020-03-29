fc2d5212e3bfca3947b794f3ca14dbd648b2af15
jansenicus <jansen.simanullang@gmail.com>
2020-03-29 14:46:14 +0700

    svelte-site: index.svelte dan translation.json

--- repositories/sveltejs/svelte/site/src/routes/index.svelte
+++ repositories/sveltejs/svelte/site/src/routes/index.svelte
@@ -27,7 +27,7 @@
 </style>
 
 <svelte:head>
-	<title>Svelte • Cybernetically enhanced web apps</title>
+	<title> Svelte • Aplikasi web yang disempurnakan secara sibernetik </title>
 
 	<meta name="twitter:title" content="Svelte">
 	<meta name="twitter:description" content="Cybernetically enhanced web apps">
@@ -36,85 +36,84 @@
 
 <Hero
 	title="Svelte"
-	tagline="Cybernetically enhanced web apps"
+	tagline="Aplikasi web yang disempurnakan secara sibernetik"
 	outline="svelte-logo-outline.svg"
 	logotype="svelte-logotype.svg"
 />
 
 <Blurb>
 	<a href="blog/write-less-code" slot="one">
-		<h2>Write less code</h2>
-		<p>Build boilerplate-free components using languages you already know — HTML, CSS and JavaScript</p>
+		<h2> Tulis lebih sedikit kode </h2>
+		<p> Buat komponen bebas-boilerplate menggunakan bahasa yang sudah Anda ketahui - HTML, CSS, dan JavaScript </p>
 
 		<span class="learn-more">learn more</span>
 	</a>
 
 	<a href="blog/virtual-dom-is-pure-overhead" slot="two">
-		<h2>No virtual DOM</h2>
-		<p>Svelte compiles your code to tiny, framework-less vanilla JS — your app starts fast and stays fast</p>
+		<h2> Tidak ada DOM virtual </h2>
+		<p> Svelte mengkompilasi kode Anda menjadi vanilla JS yang kecil tanpa kerangka - aplikasi Anda mulai dengan cepat dan tetap cepat </p>
 
 		<span class="learn-more">learn more</span>
 	</a>
 
 	<a href="blog/svelte-3-rethinking-reactivity" slot="three">
-		<h2>Truly reactive</h2>
-		<p>No more complex state management libraries — Svelte brings reactivity to JavaScript itself</p>
+		<h2> Benar-benar reaktif </h2>
+		<p> Tidak ada lagi pustaka manajemen <i>state</i> yang rumit - Svelte menghadirkan reaktivitas ke dalam JavaScript itu sendiri </p>
 
 		<span class="learn-more">learn more</span>
 	</a>
 
 	<div class="description" slot="what">
-		<p>Svelte is a radical new approach to building user interfaces. Whereas traditional frameworks like React and Vue do the bulk of their work in the <em>browser</em>, Svelte shifts that work into a <em>compile step</em> that happens when you build your app.</p>
+		<p> Svelte adalah pendekatan baru yang radikal untuk membangun antarmuka pengguna. Sementara kerangka kerja tradisional seperti React dan Vue melakukan sebagian besar pekerjaan mereka di <em> browser </em>, Svelte menggeser pekerjaan itu ke dalam <em> langkah kompilasi </em> yang terjadi ketika Anda membangun aplikasi Anda. </p>
 
-		<p>Instead of using techniques like virtual DOM diffing, Svelte writes code that surgically updates the DOM when the state of your app changes.</p>
+		<p> Alih-alih menggunakan teknik seperti DOM virtual, Svelte menulis kode yang memperbarui DOM dengan presisi yang tinggi ketika keadaan aplikasi Anda berubah.</p>
 
-		<p><a href="blog/svelte-3-rethinking-reactivity">Read the introductory blog post</a> to learn more.</p>
+		<p> <a href="blog/svelte-3-rethinking-reactivity"> Baca posting blog pengantar </a> untuk mempelajari lebih lanjut. </p>
 	</div>
 
 	<div style="grid-area: start; display: flex; flex-direction: column; min-width: 0" slot="how">
 		<pre class="language-bash" style="margin: 0 0 1em 0; min-width: 0; min-height: 0">
 npx degit <a href="https://github.com/sveltejs/template" style="user-select: initial;">sveltejs/template</a> my-svelte-project
-<span class="token comment"># or download and extract <a href="https://github.com/sveltejs/template/archive/master.zip">this .zip file</a></span>
+<span class="token comment"># atau unduh dan ekstrak <a href="https://github.com/sveltejs/template/archive/master.zip">file .zip</a> ini</span>
 cd my-svelte-project
 
 npm install
 npm run dev
 		</pre>
 
-		<p style="flex: 1">See the <a href="blog/the-easiest-way-to-get-started">quickstart guide</a> for more information.</p>
+		<p style = "flex: 1"> Lihat <a href="blog/the-easiest-way-to-get-started"> panduan mulai cepat </a> untuk informasi lebih lanjut. </p>
 
-		<p class="cta"><a rel="prefetch" href="tutorial">Learn Svelte</a></p>
+		<p class = "cta"> <a rel="prefetch" href="tutorial"> Pelajari Svelte </a> </p>
 	</div>
 </Blurb>
 
 <div class="examples">
 	<Example id="hello-world">
-		<p>Svelte components are built on top of HTML. Just add data.</p>
+		<p>Komponen Svelte dibangun di atas HTML. Cukup tambahkan data.</p>
 	</Example>
 
 	<Example id="nested-components">
-		<p>CSS is component-scoped by default — no more style collisions or specificity wars. Or you can <a href="/blog/svelte-css-in-js">use your favourite CSS-in-JS library</a>.</p>
+		<p> CSS dibatasi komponennya secara default - tidak ada lagi benturan gaya atau perang kekhususan. Atau Anda dapat <a href="/blog/svelte-css-in-js"> menggunakan perpustakaan CSS-in-JS favorit Anda </a>. </p>
 	</Example>
 
 	<Example id="reactive-assignments">
-		<p>Trigger efficient, granular updates by assigning to local variables. The compiler does the rest.</p>
+		<p>Memicu pembaruan granular yang efisien dengan penetapan (<i>assignment</i>) variabel lokal. <i>Compiler</i> mengerjakan sisanya.</p>
 	</Example>
 
 	<Example id="svg-transitions">
-		<p>Build beautiful UIs with a powerful, performant transition engine built right into the framework.</p>
+		<p>Membangun UI yang indah dengan mesin transisi yang kuat dan berkinerja tinggi yang dibangun langsung pada kerangka kerja.</p>
 	</Example>
 </div>
 
 <Section>
-	<h3>Who's using Svelte?</h3>
+	<h3>Siapa yang menggunakan Svelte?</h3>
 
 	<WhosUsingSvelte/>
 </Section>
 
 <Section>
-	<h3>Contributors</h3>
+	<h3>Para Kontributor</h3>
 
-	<p>Svelte is free and open source software, made possible by the work of dozens of volunteers. <a href="https://github.com/sveltejs/svelte">Join us!</a></p>
-
+<p> Svelte adalah perangkat lunak gratis dan <i>open source</i>, dimungkinkan oleh karya lusinan sukarelawan. <a href="https://github.com/sveltejs/svelte"> Bergabunglah dengan kami! </a> </p>
 	<Contributors/>
 </Section>
